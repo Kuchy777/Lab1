da4a44b6fafaf3066e83290765ef634caf19daaa
Jakub Kucharski <jakub.kucharski36@gmail.com>
2025-10-07 08:00:59 +0000

    Com1_1.2

--- script.js
+++ script.js
@@ -11,11 +11,24 @@
     ex1_content.innerHTML = tabela.toString();
   });
 
+  // Ex2
 
+  document.getElementById('ex2_text').addEventListener('input', function() {
+    const value = this.value;
+    const content = document.getElementById('ex2_content');
 
+    if (/[a-zA-Z]/.test(value)) {
+      content.textContent = 'Numer nie może zawierać liter';
+    }
+    else if (/[^0-9]/.test(value)) {
+      content.textContent = 'Numer nie może zawierać znaków specjalnych';
+    }
+    else if (value.length !== 9) {
+      content.textContent = 'Numer musi zawierać 9 cyfr';
+    }
+    else {
+      content.textContent = 'Numer jest poprawny';
+    }
+  });
 
-
-
-
-
-})();
+})();   
