af483ef8f164b098c5920255fc22eb4c3e74cf18
Jakub Kucharski <jakub.kucharski36@gmail.com>
2025-10-07 07:46:09 +0000

    Com1_1.1

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-(function () {
-  
+(function() {
+  var ex1_button = document.getElementById('ex1_button');
+  var ex1_content = document.getElementById('ex1_content');
+
+  // Ex1
+  ex1_button.addEventListener('click', function() {
+    var tabela = [];
+    for (var i = 0; i <= 9; i++) {
+      tabela.push(i);
+    }
+    ex1_content.innerHTML = tabela.toString();
+  });
+
+
+
+
+
+
+
+
 })();
